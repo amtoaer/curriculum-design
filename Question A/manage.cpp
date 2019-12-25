50665f2cd9c20f10e05537bb0afe9d6bf363b147
JeasonLau <b1361974998@gmail.com>
2019-12-25 16:23:54 +0800

    day 2

--- Question A/manage.cpp
+++ Question A/manage.cpp
@@ -62,6 +62,57 @@ void Product::Removal(int a)
     p->amount -= a;
 }
 
+void Product::ChangeProduct()
+//修改商品信息
+{
+    int i = 0, choice[4] = { 0 }, expirationDat;
+    string nam, sor;
+    float pric;
+    char c;
+    cout << "1、修改姓名\n2、修改商品类别\n3、修改商品价格\n4、修改商品保质期\n请输入你想修改的信息选项（可多选，以空格分隔）：" << endl;
+    cin >> choice[i];
+    while (1) {
+        if (choice[i] < 1 || choice[i] > 4) {
+            cout << "输入错误，请重新输入！" << endl;
+            i = 0;
+            cin >> choice[i];
+        } else {
+            i++;
+            c = getchar();
+            if (c == '\n')
+                break;
+            cin >> choice[i];
+        }
+    }
+    for (int i = 0; choice[i] != 0; i++) {
+        switch (choice[i]) {
+        case 1:
+            cout << "将该姓名修改为：";
+            cin >> nam;
+            name = nam;
+            break;
+        case 2:
+            cout << "将商品类别修改为：";
+            cin >> sor;
+            sort = sor;
+            break;
+        case 3:
+            cout << "将商品价格修改为：";
+            cin >> pric;
+            price = pric;
+            break;
+        case 4:
+            cout << "将商品保质期修改为：";
+            cin >> expirationDat;
+            expirationDate = expirationDat;
+            break;
+        default:
+            cout << "输入错误！\n";
+            break;
+        }
+    }
+}
+
 void Product::SaveProduct(ofstream& save)
 {
     //将该商品的所有信息输出到文件
